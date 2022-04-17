ec2112c6ce80483c791726035bdcbf722491eeed
farahdilla <farahdilla18ti@mahasiswa.pcr.ac.id>
2022-04-17 22:41:11 +0700

    Initial commit

---
+++ app/src/main/java/com/icha/livedatawithapi/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.icha.livedatawithapi
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.ArrayAdapter
+import androidx.activity.viewModels
+import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
+import com.icha.livedatawithapi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val mainViewModel: MainViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        supportActionBar?.hide()
+        mainViewModel.restaurant.observe(this, { restaurant ->
+            activityMainBinding.tvTitle.text = restaurant.name
+            activityMainBinding.tvDescription.text = restaurant.description
+            Glide.with(this)
+                    .load("https://restaurant-api.dicoding.dev/images/large/${restaurant.pictureId}")
+                    .into(activityMainBinding.ivPicture)
+        })
+
+        mainViewModel.listReview.observe(this, {consumerReviews ->
+            val listReview = consumerReviews.map {
+                "${it.review}\n- ${it.name}"
+            }
+
+            activityMainBinding.lvReview.adapter =
+                    ArrayAdapter(this, R.layout.item_review, listReview)
+            activityMainBinding.edReview.setText("")
+        })
+
+        mainViewModel.isLoading.observe(this, {
+            activityMainBinding.progressBar.visibility = if (it) View.VISIBLE else View.GONE
+        })
+
+        mainViewModel.snackbarText.observe(this, {
+            it.getContentIfNotHandled()?.let {snackBarText->
+                Snackbar.make(
+                        window.decorView.rootView,
+                        snackBarText,
+                        Snackbar.LENGTH_SHORT
+                ).show()
+            }
+
+        })
+
+        activityMainBinding.btnSend.setOnClickListener { view->
+            mainViewModel.postReview(activityMainBinding.edReview.text.toString())
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+}
